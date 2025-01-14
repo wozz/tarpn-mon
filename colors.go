92319e0c59f5b09cc7923abef942ce2dbae2c4be
user <wozz@koh.ms>
2025-01-13 22:53:01 -0500

    update

--- colors.go
+++ colors.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"crypto/sha1"
+	"crypto/md5"
 	"encoding/hex"
 	"fmt"
 )
 
 // hashCallsign takes a callsign and returns an HSL color as a string
 func hashCallsign(callsign string) string {
-	hash := sha1.Sum([]byte(callsign))
+	hash := md5.Sum([]byte(callsign))
 	hexHash := hex.EncodeToString(hash[:6])
 	var hue int
 	fmt.Sscanf(hexHash, "%x", &hue)
